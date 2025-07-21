5069f1c7dd54d065292b13e9187b2e69c115222d
Virginia Morrone <vikymorrone@gmail.com>
2025-07-21 16:09:21 -0300

    Con html

--- adivina.js
+++ adivina.js
@@ -1,33 +1,51 @@
-// Número secreto definido como constante
-const numeroSecreto = 7;
+function adivinarNumero(){
+    // Número secreto definido como constante
+    const numeroSecreto = 7;
 
-// Contador de intentos
-let intentos = 0;
+    // Contador de intentos
+    let intentos = 0;
 
-// Máximo de intentos permitidos
-const maxIntentos = 3;
+    // Máximo de intentos permitidos
+    const maxIntentos = 3;
 
-// Variable para guardar el intento del usuario
-let intento = null;
+    // Variable para guardar el intento del usuario
+    let intento = null;
 
-// Mientras no adivine y tenga intentos disponibles
-while (intento !== numeroSecreto && intentos < maxIntentos) {
-  // Pedir al usuario que ingrese un número
-  intento = Number(prompt("Adivina el número (del 1 al 10):"));
+    // Mientras no adivine y tenga intentos disponibles
+    while (intento !== numeroSecreto && intentos < maxIntentos) {
+    // Pedir al usuario que ingrese un número
+    intento = Number(prompt("Adivina el número (del 1 al 10):"));
 
-  // Aumentar el contador
-  intentos++;
+    // Aumentar el contador
+    intentos++;
 
-  // Verificar si adivinó o no
-  if (intento === numeroSecreto) {
-    alert("¡Felicitaciones! Adivinaste el número.");
-  } else if (intentos < maxIntentos) {
-    alert("No es el número correcto. Te quedan " + (maxIntentos - intentos) + " intento(s).");
-  }
+    // Verificar si adivinó o no
+    if (intento === numeroSecreto) {
+        alert("¡Felicitaciones! Adivinaste el número.");
+    } else if (intentos < maxIntentos) {
+        alert("No es el número correcto. Te quedan " + (maxIntentos - intentos) + " intento(s).");
+    }
+    }
+
+    // Si se acabaron los intentos y no adivinó
+    if (intento !== numeroSecreto) {
+    alert("Lo siento, se acabaron tus intentos. El número correcto era " + numeroSecreto + ".");
+    }
 }
 
-// Si se acabaron los intentos y no adivinó
-if (intento !== numeroSecreto) {
-  alert("Lo siento, se acabaron tus intentos. El número correcto era " + numeroSecreto + ".");
+//adivinarNumero();
+
+function adivinarPorjs(){
+    let datoEntrada = parseInt(document.getElementById("dato").value);
+    if(datoEntrada === 7){
+        document.getElementById("p-info").textContent = "Felicitaciones, adivinaste!"
+    }
+    else{
+       document.getElementById("p-info").textContent = "Perdiste, vuelve a intentarlo!"
+    }
 }
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("btn-enviar").addEventListener("click",adivinarPorjs);
+})
+
 
